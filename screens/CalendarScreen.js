f2fb4cb1872d27adc72b3ce099b9a3e1764b31be
Tom Haight <tomhaight@Toms-MacBook-Pro.local>
2020-03-27 21:15:00 -0500

    Made some adjustments to time

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -8,26 +8,31 @@ import { Colors } from '../constants/Colors';
 
 const CalendarScreen = props => {
     
-    
+    //Calculates the current time with adjustment for timezone for use in graying out calendar dates
+    const date = new Date();
+    const timeOffset = date.getTimezoneOffset() * 60000;
+    const currentDate = Date.now()-timeOffset;
+
     return (
 
         <View style={styles.container}>
             <View style={{ alignSelf: 'flex-start' }}><RichHeader title='School Events' /></View>
-            <View style={styles.calendarContainer}><Agenda minDate={Date.now}
-                items={{ '2020-03-26': [{ name: 'item 1 - any js object' }] }}
+            <View style={styles.calendarContainer}><Agenda minDate={currentDate}
+                items={{ '2020-03-26': [{ name: 'item 1 - any js object' }],
+                    '2020-03-27': [{ name: 'item 1 - any js object' }] }}
                 renderItem={(items) => { return <View><Text>{items.name}</Text></View> }}
                 renderEmptyDate={() => { return (<View />) }}
                 
                 theme={{
                     selectedDayBackgroundColor: '#E1BC41', //accent
                     selectedDayTextColor: '#4C1E52', //dark accent
-                    dotColor: '#7A5580', //primary
                     selectedDotColor: '#4C1E52', //dark accent 
-                    todayTextColor: '#E1BC41', //accent
+                    dotColor: '#7A5580', //primary
+                    todayTextColor: '#3500FF', //accent
                     dayTextColor: '#7A5580', //primary
                     agendaDayTextColor: '#4C1E52',
                     agendaKnobColor: '#4C1E52',
-                    agendaTodayColor: '#E1BC41',
+                    agendaTodayColor: '#3500FF',
                     agendaDayNumColor: '#7A5580'
                 }}
             />
@@ -38,7 +43,7 @@ const CalendarScreen = props => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1
         /*         alignContent: 'center',
                 justifyContent: 'center', */
     },
--- screens/LunchScreen.js
+++ screens/LunchScreen.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import RichHeader from '../components/RichHeader';
+import { Agenda } from 'react-native-calendars';
 
 
 const LunchScreen = props => {
+    
+    const date = new Date();
+    const timeOffset = date.getTimezoneOffset() * 60000;
+    const currentDate = Date.now()-timeOffset;
+    
     return (
         <View style={styles.container}>
-            <View style={{alignSelf: 'flex-start'}}><RichHeader title='Lunches' /></View>
-            <Text style={{textAlign: 'center'}}>This is a lunch!</Text>
+            <View style={{ alignSelf: 'flex-start' }}><RichHeader title='Lunch' /></View>
+            <View style={styles.calendarContainer}><Agenda minDate={currentDate}
+                items={{ '2020-03-27': [{ name: 'item 3 - js object' }],
+                    '2020-03-27': [{ name: 'item 1 - any js object' }],
+                    '2020-03-28': [{ name: 'item 2 - any js object' }],
+                    '2020-03-29': []}}
+                renderItem={(items) => { return <View><Text>{items.name}</Text></View> }}
+                renderEmptyDate={() => { return (<View />) }}
+                
+                theme={{
+                    selectedDayBackgroundColor: '#E1BC41', //accent
+                    selectedDayTextColor: '#4C1E52', //dark accent
+                    selectedDotColor: '#4C1E52', //dark accent 
+                    dotColor: '#7A5580', //primary
+                    todayTextColor: '#3500FF', //accent
+                    dayTextColor: '#7A5580', //primary
+                    agendaDayTextColor: '#4C1E52',
+                    agendaKnobColor: '#4C1E52',
+                    agendaTodayColor: '#3500FF',
+                    agendaDayNumColor: '#7A5580'
+                }}
+            />
+            </View>
         </View>
     )
 };
@@ -15,6 +42,12 @@ const LunchScreen = props => {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+        /*         alignContent: 'center',
+                justifyContent: 'center', */
+    },
+    calendarContainer: {
+        flex:1,
+        paddingBottom: 20
     }
 });
 
